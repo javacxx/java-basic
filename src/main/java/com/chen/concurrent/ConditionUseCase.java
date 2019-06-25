863c1afd4c0f0541d3f65c2239d7864752827585
javacxx <xx.chen@foxmail.com>
2019-06-25 11:37:43 +0800

    condition use

---
+++ src/main/java/com/chen/concurrent/ConditionUseCase.java
@@ -0,0 +1,37 @@
+package com.chen.concurrent;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author CXX
+ * @date 2019/6/25 11:28
+ */
+public class ConditionUseCase {
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+    int count;
+    public void conditionWait() throws InterruptedException {
+        lock.lock();
+        try {
+            condition.await();
+            // do something
+            count--;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void conditionSignal() {
+        lock.lock();
+        try {
+            // do something
+            count++;
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
